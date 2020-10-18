a6b4c0ebe319e0f7067760973c99cc55dd88ab6e
learnJJ <gayatri.ponnaganti@gmail.com>
2020-10-18 14:35:24 +0530

    4 files modified, 1 added

---
+++ app/menu/menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core";
+import { Dish } from "../shared/dish";
+
+@Component({
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.scss"]
+})
+export class MenuComponent implements OnInit {
+  constructor() {}
+
+  dishes: Dish = [
+    {
+      id: '1',
+      name: "John",
+      image: "",
+      category: "123",
+      featured: "123",
+      price: "123",
+      description: "123"
+    }
+  ];
+  ngOnInit() {}
+}
