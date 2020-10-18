dd4360ce49d64d57b84a0dbc42b18934ec0c2e4c
learnJJ <gayatri.ponnaganti@gmail.com>
2020-10-18 22:13:30 +0530

    1 file modified

--- app/app-routing/routes.ts
+++ app/app-routing/routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from "@angular/router";
 
 import { MenuComponent } from "../menu/menu.component";
-import { DishdetailComponent } from "../dishdetail/dishdetail.component";
+import { DishDetailComponent } from "./dish-detail/dish-detail.component";
 import { HomeComponent } from "../home/home.component";
 import { AboutComponent } from "../about/about.component";
 import { ContactComponent } from "../contact/contact.component";
