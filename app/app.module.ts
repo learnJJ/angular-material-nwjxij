d179e351499e3b524d486c11e47b45114a9ae7cc
learnJJ <gayatri.ponnaganti@gmail.com>
2020-10-18 14:50:38 +0530

    7 binaries uploaded

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material";
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatListModule } from "@angular/material/list";
 
 import { AppComponent } from "./app.component";
 import { MenuComponent } from "./menu/menu.component";
@@ -16,7 +17,8 @@ import "hammerjs";
     FormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
   ],
   declarations: [AppComponent, MenuComponent],
   providers: [],
--- app/menu/menu.component.ts
+++ app/menu/menu.component.ts
@@ -11,7 +11,25 @@ export class MenuComponent implements OnInit {
 
   dishes: Dish = [
     {
-      id: '1',
+      id: "1",
+      name: "John",
+      image: "",
+      category: "123",
+      featured: "123",
+      price: "123",
+      description: "123"
+    },
+    {
+      id: "1",
+      name: "John",
+      image: "",
+      category: "123",
+      featured: "123",
+      price: "123",
+      description: "123"
+    },
+    {
+      id: "1",
       name: "John",
       image: "",
       category: "123",
