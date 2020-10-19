a6264790665ac5e4e676d449b538e60d5d2eace6
learnJJ <gayatri.ponnaganti@gmail.com>
2020-10-19 08:20:08 +0530

    5 files modified

--- app/app-routing/routes.ts
+++ app/app-routing/routes.ts
@@ -8,6 +8,8 @@ import { ContactComponent } from "../contact/contact.component";
 
 export const routes: Routes = [
   { path: "home", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "contact", component: ContactComponent },
   { path: "menu", component: MenuComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" }
 ];
